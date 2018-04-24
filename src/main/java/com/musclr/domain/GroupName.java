4e1c85ee9edda0b5670359c98db5c13fe7102768
Chris <chris_holc@hotmail.fr>
2018-04-24 22:32:14 +0200

    introduced the group notion

---
+++ src/main/java/com/musclr/domain/GroupName.java
@@ -0,0 +1,11 @@
+package com.musclr.domain;
+
+public class GroupName {
+    public static final String USER = "users";
+    public static final String EVENT = "events";
+    public static final String TOWN = "towns";
+    public static final String GYM = "gyms";
+
+    private GroupName() {
+    }
+}
---
+++ src/main/java/com/musclr/domain/RelationName.java
@@ -0,0 +1,10 @@
+package com.musclr.domain;
+
+public class RelationName {
+    public static final String FRIEND = "friend";
+    public static final String COACH = "coach";
+    public static final String PARTICIPATE = "participate";
+
+    private RelationName() {
+    }
+}
